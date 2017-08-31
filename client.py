d3610985beb9b0e1b643ebfb17847a008b06f26c
Daniel Pollithy <daniel.pollithy@gmail.com>
2017-08-31 14:15:02 +0200

    rssi

--- client.py
+++ client.py
@@ -149,11 +149,9 @@ def run():
     while connecting:
         nearby_devices = bluetooth.discover_devices(duration=1, flush_cache=False)
         print("found %d devices" % len(nearby_devices))
-        print(nearby_devices)
-        print(nearby_devices.items())
 
-        for address, name in nearby_devices:
-            print("  %s - %s" % (address, name))
+        for address in nearby_devices:
+            print("  %s " % (address))
             if address.lower() == settings.PEER_BT_ADDRESS:
                 print("PEERING PARTNER FOUND")
                 try:
