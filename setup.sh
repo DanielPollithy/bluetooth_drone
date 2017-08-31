489d1b47ef0ead4ba88bfcc857b911b5b2658e01
Daniel Pollithy <daniel.pollithy@gmail.com>
2017-08-31 11:48:03 +0200

    resetup raspy

--- setup.sh
+++ setup.sh
@@ -1,23 +1,28 @@
 sudo apt-get install python-pip python-dev ipython
 sudo apt-get install bluetooth libbluetooth-dev
 
+sudo pip install pybluez
+# Install python package
+git clone https://github.com/ewenchou/bluetooth-proximity.git
+cd bluetooth-proximity
+sudo python setup.py install
+
 
 
 
 # for low power bluetooth development
-sudo apt-get install libboost-thread-dev
-sudo apt-get install libperl-dev
+# sudo apt-get install libboost-thread-dev
+# sudo apt-get install libperl-dev
 # was necessary
-sudo apt-get update --fix-missing
-sudo apt-get install libgtk2.0-dev
-sudo pip install gattlib
-
+# sudo apt-get update --fix-missing
+# sudo apt-get install libgtk2.0-dev
+# sudo pip install gattlib
 # sudo apt-get install mercurial
 # hg clone https://bitbucket.org/OscarAcena/pygattlib
 # cd pygattlib
 
-sudo pip install pybluez
-# TODO: install py proximity
+
+
 
 
 git clone https://github.com/ewenchou/bluetooth-proximity.git
