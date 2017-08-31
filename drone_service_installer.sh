ab3a9ad7b80b502c5b152160d81340a9dddcd068
Daniel Pollithy <daniel.pollithy@gmail.com>
2017-08-31 22:54:38 +0200

    d3 update

--- drone_service_installer.sh
+++ drone_service_installer.sh
@@ -1,2 +1,3 @@
 cp drone.service /etc/systemd/system/drone.service
+sudo chmod 644 /etc/systemd/system/drone.service
 systemctl enable drone.service
--- station_service_installer.sh
+++ station_service_installer.sh
@@ -1,2 +1,3 @@
 cp station.service /etc/systemd/system/station.service
+sudo chmod 644 /etc/systemd/system/station.service
 systemctl enable station
