11b0a932609c47c684840af4e4892a6aa9aa5bce
Daniel Pollithy <daniel.pollithy@gmail.com>
2017-09-01 12:37:09 +0200

    d3 update

--- relais.py
+++ relais.py
@@ -2,10 +2,10 @@ from subprocess import Popen
 
 
 def switch_off():
-    popen = Popen(['sudo', './touch_switch', 'off'])
+    popen = Popen(['sudo', 'bash', '/home/linaro/bluetooth_drone/touch_switch', 'off'])
     popen.wait()
 
 
 def switch_on():
-    popen = Popen(['sudo', './touch_switch', 'on'])
+    popen = Popen(['sudo', 'bash', '/home/linaro/bluetooth_drone/touch_switch', 'on'])
     popen.wait()
